a00515a7e1dcec7331314771759ce70c44bf94e1
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2019-04-27 11:33:08 +0900

    feat: add parsable token

--- compiler/9cc.c
+++ compiler/9cc.c
@@ -122,7 +122,7 @@ void tokenize(char *p) {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-') {
+		if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
 			tokens[i].ty = *p;
 			tokens[i].input = p;
 			i++;
